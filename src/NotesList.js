925bb6f1bb5f9c14ed8dc7ce071a50de7a99e158
Ian Long <singlespider4@gmail.com>
2024-11-03 23:58:18 -0500

    Finished project

---
+++ src/NotesList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Note from "./Notes.js";
+
+const NotesList = (props) => {
+  const keepSearchMatches = (note) => note.doesMatchSearch;
+  const searchMatches = props.notes.filter(keepSearchMatches);
+
+  const renderNote = (note) => (
+    <Note removeNote={props.removeNote} note={note} key={note.id} onType={props.onType} />
+  );
+
+  const noteElements = searchMatches.map(renderNote);
+  return <ul className="notes-list">{noteElements}</ul>;
+};
+
+export default NotesList;
